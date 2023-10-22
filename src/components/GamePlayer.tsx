6130b2fc9ddbf3d52bd5aa870072fe92e5dac769
Junkie <LAcase214@gmail.com>
2023-10-22 19:51:26 -0400

    Separate type definitions, change headers, use fra

--- src/components/GamePlayer.tsx
+++ src/components/GamePlayer.tsx
@@ -1,78 +1,59 @@
 import { ReactNode, createContext, useRef, useState } from "react";
 
 type GameplayerType = {
-    setInternal: React.Dispatch<React.SetStateAction<string | undefined>>
-    setExternal: React.Dispatch<React.SetStateAction<string | undefined>>
-    setVisible: React.Dispatch<React.SetStateAction<boolean>>
-}
+    setInternal: React.Dispatch<React.SetStateAction<string | undefined>>;
+    setExternal: React.Dispatch<React.SetStateAction<string | undefined>>;
+    setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 export const GameplayerCtx = createContext<GameplayerType | null>(null);
 
-export default function GamePlayer({
-    children
-} : {
-    children: ReactNode
-}) {
-    const [internal, setInternal] = useState<string | undefined>(undefined);
-    const [external, setExternal] = useState<string | undefined>(undefined);
-
+export default function GamePlayer({ children }: { children: ReactNode }) {
+    const [internal, setInternal] = useState<string | undefined>();
+    const [external, setExternal] = useState<string | undefined>();
     const [visible, setVisible] = useState(false);
-    
     const [fullScreen, setFullScreen] = useState(false);
 
-    const iframe = useRef<HTMLIFrameElement | null>(null);
-    const canvas = useRef<HTMLCanvasElement | null>(null);
+    const iframe = useRef<HTMLIFrameElement>(null);
+    const canvas = useRef<HTMLCanvasElement>(null);
+
+    const contextValue = {
+        setInternal,
+        setExternal,
+        setVisible,
+    };
 
     return (
-        <GameplayerCtx.Provider value={{
-            setInternal: setInternal,
-            setExternal: setExternal,
-            setVisible: setVisible
-        }}>
+        <GameplayerCtx.Provider value={contextValue}>
             {visible && (
-                <div className="gameplayer">
-                    <div>
-                            <h2>Game Player</h2>
-                            <div
-                                onClick={() => {
-                                    setVisible(false);
+                <>
+                    <div className="gameplayer-header">
+                        <h2>Game Player</h2>
+                        <div onClick={() => setVisible(false)}>X</div>
+                    </div>
+                    <div className="gameplayer-content">
+                        {internal && <canvas ref={canvas}></canvas>}
+                        {external && (
+                            <iframe src={external} allowFullScreen ref={iframe}></iframe>
+                        )}
+                        <div className="mx-auto">
+                            <button
+                                onClick={async () => {
+                                    if (canvas.current) {
+                                        await canvas.current.requestFullscreen();
+                                    } else if (iframe.current) {
+                                        await iframe.current.requestFullscreen();
+                                    }
+                                    setFullScreen(true);
                                 }}
+                                className="button"
                             >
-                                X
-                            </div>
+                                Full Screen
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                            {internal && (
-                                <canvas ref={canvas}>
-                                </canvas>
-                            )}
-                            {external && (
-                                <iframe
-                                    src={external}
-                                    allowFullScreen={true}
-                                    ref={iframe}
-                                />
-                            )}
-                            <div className="mx-auto">
-                                <button
-                                    onClick={async () => {
-                                        const c = canvas.current;
-                                        const i = iframe.current;
-                                        
-                                        if (c)
-                                            await c.requestFullscreen();
-                                        else if (i)
-                                            await i.requestFullscreen();
-
-                                        setFullScreen(true);
-                                    }}
-                                    className="button"
-                                >Full Screen</button>
-                            </div>
-                    </div>
-                </div>
+                </>
             )}
-
             {children}
         </GameplayerCtx.Provider>
     );
